7cf96774ff43263ae92c29b1163161fc6a948faa
guguinhaaah <gustavo.melo.fla@gmail.com>
2026-09-24 05:17:36 -0300

    style(config): fix formatting and whitespaces according to ruff rules

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,17 +1,19 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     PROJECT_NAME: str = "API - Carteira Estudantil IFPE"
     API_V1_STR: str = "/api/v1"
-    
+
     DATABASE_URL: str
     SECRET_KEY: str
-    
+
     ALGORITHM: str = "HS256"
     QR_CODE_EXPIRE_SECONDS: int = 180
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
+
 
 settings = Settings()
